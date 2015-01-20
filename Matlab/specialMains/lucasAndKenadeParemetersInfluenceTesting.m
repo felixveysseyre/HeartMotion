ebdc94d3e97ad7ef936952f70d2b26816f0abea6
Felix VEYSSEYRE <felix.veysseyre@gmail.com>
2015-01-20 17:12:37 +0100

    Implemented main to displaying influence of Lucas & Kenade parameters

---
+++ Matlab/specialMains/lucasAndKenadeParemetersInfluenceTesting.m
@@ -0,0 +1,54 @@
+clc;
+clear all;
+close all;
+
+%% Paths %%
+
+addpath('../sequence');
+
+addpath('../tools');
+addpath('../displaying');
+addpath('../specialDisplaying');
+
+addpath('../hornAndSchunck');
+addpath('../lucasAndKanade');
+addpath('../blockMatching');
+
+addpath('../testingSequenceGenerator');
+addpath('../external');
+
+%% Read images %%
+
+imagesSequence = sequence();
+imagesSequence.fillFromTestingSequence('../../Data/', 'TestingSequence3', Inf);
+
+%% Lucas & Kanades - block size &  paremeter influence testing %%
+
+blockSizes = {1, 3, 5, 7};
+windowTypes = {'uniform', 'hamming', 'gaussian'};
+
+velocitiesXSequences = [];
+velocitiesYSequences = [];
+
+for i = 1:1:length(blockSizes)
+    
+    blockSize = blockSizes{i};
+    
+    for j = 1:1:length(windowTypes)
+        
+        windowType = windowTypes{j};
+        
+        [velocitiesXSequence, velocitiesYSequence] = lucasAndKenade(imagesSequence, blockSize, windowType);
+    
+        velocitiesXSequences{i, j} = velocitiesXSequence;
+        velocitiesYSequences{i, j} = velocitiesYSequence;
+        
+    end
+    
+end
+
+%% Display %%
+
+overlaying = 1;
+
+displayLucasAndKenadeParemetersInfluence(imagesSequence, velocitiesXSequences, velocitiesYSequences, blockSizes, windowTypes, overlaying);
